9f3f746318b7fcebd8e160a01b3878c31bfd7305
Giacomo Patella <giacomopatella@gmail.com>
2014-01-06 14:32:31 +0000

    Translated ruby code in js, tests passed

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -3,10 +3,13 @@ function Player(name) {
   // console.log("New player created")
 }
 
-Player.prototype.picks = function() {
-  // if(foo === bar) {
+Player.prototype.picks = function(pick) {
+  this.pick = pick;
+//   console.log()
+}
 
-  // }
+Game.prototype.samepick = function() {
+  return this.player1.pick === this.player2.pick;
 }
 
 function Game(player1, player2) {
@@ -14,6 +17,25 @@ function Game(player1, player2) {
   this.player2 = player2;
 }
 
+Game.prototype.PAIRS = {
+  rock: {beats : "scissors"},
+  paper: {beats : "rock"},
+  scissors: {beats : "paper"}
+}
+
 Game.prototype.winner = function() {
-  return this.player1;
+  if (this.samepick())
+  {
+    return null;
+  }
+
+  if(this.PAIRS[this.player1.pick]['beats'] === this.player2.pick)
+  {
+    return this.player1;
+  }
+  else
+  {
+    return this.player2;
+  }
+
 }
